d99340c58723a0dc96ab3319cef1acbc48c15ea9
JingYi <zmmouoasus@gmail.com>
2022-03-05 23:00:21 +0800

    table加上github api data - 3/5

--- githubRepoReact/static/js/index.js
+++ githubRepoReact/static/js/index.js
@@ -1,57 +1,69 @@
-$(document).ready(function() {
-    $('#table_id').DataTable({
-        // ajax: {
-        //     url: 'https://api.github.com/users/Yilongasus/repos',
-        //     data: 'json'
-        // },
-        // 這個目前沒吃到
-        columns: [
-            {
-                title: "名稱",
-                data: ''
-            },
-            {
-                title: "支持和讚許數",
-                data: ''                
+$(document).ready(function(){
+    const getGithubApi = async () => {
+        const response = await fetch('https://api.github.com/users/Dcard/repos');
+        const data = await response.json();
+
+        $('#myTable').DataTable({
+            data: data,
+            bLengthChange: false,
+            columns: [
+                { 
+                    title: 'id',
+                    data: 'id',
+                    visible: false
+                },
+                { 
+                    title: '名稱',
+                    data: 'name'
+                },
+                { 
+                    title: '支持和讚許數',
+                    data: 'stargazers_count'
+                },
+                { 
+                    title: '網址',
+                    data: 'html_url',
+                    render: function(data) {
+                        return '<a href='+ data +' target=_blank>前往觀看</a>'
+                    }
+                }
+            ],
+            lengthChange: true,
+            // 呈現左上角下拉頁數選單
+
+            lengthMenu: [10, 20, 30, 100],
+            // 頁數選單數值
+
+            pageLength: 10,
+            // 每頁預設個數
+
+            // paging: false,
+            // 要不要分頁
+
+
+            searching: true,
+            // 搜索功能是否開啟(預設開啟)
+
+            ordering: true,
+            // 排序是否開啟(預設開啟)   
+
+
+            order: [[1, 'desc']],
+            /* 
+            
+                預設排序方式
+                [0, 0]：哪個欄位。
+                [0, 1]：asc 升冪排列、desc 降冪排列。                                
+            
+            */
+
+            "oLanguage": {
+                "sUrl": "/githubRepoReact/githubRepoReact/static/zhHant.json"
             }
-        ],
-        lengthChange: true, 
-        // 呈現左上角下拉頁數選單
 
-        lengthMenu: [1, 2, 5, 7],
-        // 頁數選單數值
+        });
 
-        pageLength: 5,
-        // 每頁預設個數
+    }
 
-        // paging: false,
-        // 要不要分頁
-
-
-        searching: true,
-        // 搜索功能是否開啟(預設開啟)
-
-        ordering: true,
-        // 排序是否開啟(預設開啟)   
-
-
-        order: [[ 1, 'desc' ]],
-        /* 
-        
-            預設排序方式
-            [0, 0]：哪個欄位。
-            [0, 1]：asc 升冪排列、desc 降冪排列。                                
-        
-        */
-
-        "oLanguage": {
-            "sUrl": "/githubRepoReact/githubRepoReact/static/zhHant.json"
-        }  
-        
-    });
-
-    console.log({
-                title: "名稱",
-                data: name                
-    });
+    getGithubApi();
 });
