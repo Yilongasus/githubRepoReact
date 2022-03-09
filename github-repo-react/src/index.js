6976dfd976cbb29f5920d6079cb765a8a107c644
JingYi <zmmouoasus@gmail.com>
2022-03-09 21:54:54 +0800

    table出現, 使用MDB樣式框架 - 3/9

--- github-repo-react/src/index.js
+++ github-repo-react/src/index.js
@@ -5,23 +5,138 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { MDBDataTableV5 } from 'mdbreact';
 
-const style = {fontFamily: 'Arial', color: '#FF0000'};
+// const style = {fontFamily: 'Arial', color: '#FF0000'};
 
-function repoTable() {
-  return (
-    <table style="{ style }" className="display">
-      <tr>
-        <td>你好棒!</td>
-      </tr>
+export default function Basic() {
+  const [datatable, setDatatable] = React.useState({
+    columns: [
+      {
+        label: 'Name',
+        field: 'name',
+        width: 150,
+        attributes: {
+          'aria-controls': 'DataTable',
+          'aria-label': 'Name',
+        },
+      },
+      {
+        label: 'Position',
+        field: 'position',
+        width: 270,
+      },
+      {
+        label: 'Office',
+        field: 'office',
+        width: 200,
+      },
+      {
+        label: 'Age',
+        field: 'age',
+        sort: 'asc',
+        width: 100,
+      },
+      {
+        label: 'Start date',
+        field: 'date',
+        sort: 'disabled',
+        width: 150,
+      },
+      {
+        label: 'Salary',
+        field: 'salary',
+        sort: 'disabled',
+        width: 100,
+      },
+    ],
+    rows: [
+      {
+        name: 'Jennifer Acosta',
+        position: 'Junior Javascript Developer',
+        office: 'Edinburgh',
+        age: '43',
+        date: '2013/02/01',
+        salary: '$75',
+      },
+      {
+        name: 'Cara Stevens',
+        position: 'Sales Assistant',
+        office: 'New York',
+        age: '46',
+        date: '2011/12/06',
+        salary: '$145',
+      },
+      {
+        name: 'Hermione Butler',
+        position: 'Regional Director',
+        office: 'London',
+        age: '47',
+        date: '2011/03/21',
+        salary: '$356',
+      },
+      {
+        name: 'Lael Greer',
+        position: 'Systems Administrator',
+        office: 'London',
+        age: '21',
+        date: '2009/02/27',
+        salary: '$103',
+      },
+      {
+        name: 'Jonas Alexander',
+        position: 'Developer',
+        office: 'San Francisco',
+        age: '30',
+        date: '2010/07/14',
+        salary: '$86',
+      },
+      {
+        name: 'Shad Decker',
+        position: 'Regional Director',
+        office: 'Edinburgh',
+        age: '51',
+        date: '2008/11/13',
+        salary: '$183',
+      },
+      {
+        name: 'Michael Bruce',
+        position: 'Javascript Developer',
+        office: 'Singapore',
+        age: '29',
+        date: '2011/06/27',
+        salary: '$183',
+      },
+      {
+        name: 'Donna Snider',
+        position: 'Customer Support',
+        office: 'New York',
+        age: '27',
+        date: '2011/01/25',
+        salary: '$112',
+      },
+    ],
+  });
+
+  return <MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={datatable} />;
+}
+
+function RepoTable(){
+  return(
+    <table id="myTable" className='display'>
     </table>
   );
 }
 
+
 ReactDOM.render(
   <React.StrictMode>
     <div>
-    <repoTable/>
+    <RepoTable/>
+    <Basic></Basic>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
